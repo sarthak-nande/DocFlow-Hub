4967f00902082e6670b9b942f5f5298619a026dd
sarthak-nande <sarthu102@gmail.com>
2026-02-10 22:55:31 +0530

    Project Intialized

---
+++ src/main/java/com/docflowhub/docflow_hub/DocflowHubApplication.java
@@ -0,0 +1,13 @@
+package com.docflowhub.docflow_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocflowHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocflowHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docflowhub/docflow_hub/DocflowHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docflowhub.docflow_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocflowHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
